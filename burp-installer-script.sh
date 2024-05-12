a17696f3850229877b9dbc4447ee6c3afa1d39a6
Soroush Dalili <1798313+irsdl@users.noreply.github.com>
2024-05-13 00:51:47 +0100

    Update burp-installer-script.sh

--- burp-installer-script.sh
+++ burp-installer-script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # installing burp pro
 VERSION="2024.3.1.4"
 curl -o /tmp/burpinstall.sh -k "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$VERSION&type=Linux"
