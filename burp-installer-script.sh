664e035b523c6b079c7136df0ae8c7a63c8c472f
Soroush Dalili <1798313+irsdl@users.noreply.github.com>
2024-05-12 21:14:33 +0100

    Create burp-installer-script.sh

---
+++ burp-installer-script.sh
@@ -0,0 +1,7 @@
+# installing burp pro
+VERSION="2024.3.1.4"
+curl -o /tmp/burpinstall.sh -k "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$VERSION&type=Linux"
+chmod +x /tmp/burpinstall.sh
+/tmp/burpinstall.sh -Djava.awt.headless=true
+rm /tmp/burpinstall.sh
+
